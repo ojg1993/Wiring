84cacda82b222fc47fca36fa9eb3f25072d5f053
Jingyu <ojg1993@gmail.com>
2024-05-11 23:43:39 +0900

    chore: configure router and MUI
    - installed react-router-dom
    - configured main router with home page
    - installed mui library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,22 @@
-import "./App.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import React from "react";
 
-function App() {
-  return <></>;
-}
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+const App: React.FC = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import { Box, CssBaseline } from "@mui/material";
+
+const Home = () => {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        Home
+      </Box>
+    </>
+  );
+};
+
+export default Home;
