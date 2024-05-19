fb995ee59fc6b09d34321dde27fab34d94a69254
Jinja <ojg1993@gmail.com>
2024-05-19 17:39:14 +0900

    feat: render message histroy

--- frontend/src/components/Main/MessageInterface.tsx
+++ frontend/src/components/Main/MessageInterface.tsx
@@ -2,16 +2,37 @@ import { useState } from "react";
 import { useParams } from "react-router-dom";
 import useWebSocket from "react-use-websocket";
 import { WS_ROOT } from "../../config";
+import useCrud from "../../hooks/useCrud";
+import { Server } from "../../@types/server";
+
+interface Message {
+  sender: string;
+  content: string;
+  timestamp: string;
+}
 
 const MessageInterface = () => {
-  const [newMessage, setNewMessage] = useState<string[]>([]);
+  const [newMessage, setNewMessage] = useState<Message[]>([]);
   const [message, setMessage] = useState("");
   const { serverId, channelId } = useParams();
+  const { fetchData } = useCrud<Server>(
+    [],
+    `/messages/?channel_id=${channelId}`
+  );
 
   const socketUrl = channelId ? `${WS_ROOT}/${serverId}/${channelId}/` : null;
 
   const { sendJsonMessage } = useWebSocket(socketUrl, {
-    onOpen: () => console.log("Connected"),
+    onOpen: async () => {
+      try {
+        const data = await fetchData();
+        setNewMessage([]);
+        setNewMessage(Array.isArray(data) ? data : []);
+        console.log("Connected");
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    },
     onClose: () => console.log("Disconnected"),
     onError: (event) => console.error("error:", event),
     onMessage: (msg) => {
@@ -28,10 +49,12 @@ const MessageInterface = () => {
 
   return (
     <div>
-      {newMessage.map((msg, index) => {
+      {newMessage.map((msg: Message, index: number) => {
         return (
           <div key={index}>
+            <p>{msg.sender}</p>
             <p>{msg.content}</p>
+            <p>{msg.timestamp}</p>
           </div>
         );
       })}
